8123546e5d095798504292d9a978b22204ccea0d
Alexander Kremenets <kremenets0207@gmail.com>
2014-04-06 22:06:18 +0400

    initial commit'

---
+++ app.js
@@ -0,0 +1,19 @@
+// web.js
+var express = require("express");
+var logfmt = require("logfmt");
+var path = require("path");
+var app = express();
+
+app.use(logfmt.requestLogger());
+app.use(express.static(path.join(__dirname, '')));
+
+app.set('view engine', 'jade');
+
+app.get('/', function(req, res) {
+  res.render('index.html');
+});
+
+var port = Number(process.env.PORT || 5000);
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
